2e6ce918bd0db87ebecd85b4c62db11c8a9abfa0
“cristopher” <curibe26@aoiths.org>
2026-04-27 13:37:28 -0400

    update

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-
 mapboxgl.accessToken = config.MAPBOX_API;
 
 const map = new mapboxgl.Map({
-	container: 'map',
-	style: 'mapbox://styles/mapbox/streets-v11',
-	center: [-74.5, 40],
-	zoom: 9
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-74.006, 40.7128],
+    zoom: 11
 });
+
+map.addControl(new mapboxgl.NavigationControl());
+
+const geocoder = new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken,
+    mapboxgl: mapboxgl,
+    placeholder: "Search for a location (NYC)..."
+});
+
+document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
